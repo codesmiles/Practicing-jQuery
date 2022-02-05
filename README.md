192f5f0487e6a28297fb3f64993b6736864dcc84
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-02-05 12:08:36 +0100

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+# DEEP NOTE
+
+Just in case you are wondering, this is a github repository about what I could pick up when learning jQuery.
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-$('h1').css('color', 'red');
-console.log($('h1').css('color'));
+// getting elements
+$('h1')
 
+// JQuery event isteners and how they work
 $("h1").click(() => {
     $("h1").hasClass("problemChild") === true
         ? $("h1").removeClass("problemChild")
         : $("h1").addClass("problemChild")
 });
 
+// .css sets and shows css properties
+$("h1").css("color", "red");
+// console.log($('h1').css('color')); // returns red
 
+//.attr creates and set attributes, 
 // $('button').attr('class','btn');
-
-//.attr creates and set attributes, .css sets and shows css properties 
 $('img').attr('src','app-features-boshtech.png').css('width','100%');
